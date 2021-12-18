8298f981e6da0b99fe5b474927e267ee5a1e9195
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2021-12-17 23:27:51 -0700

    about to implement router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './App.css'
-import MainList from './MainList'
-import Generate from './Generate'
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css' // THIS IS WHAT YOU NEED FOR THE BOOTSTRAP TO WORK
 import HomeScreen from './HomeScreen'
 
 function App() {
-  useEffect(() => {}, [])
-
-  const [pokelist, setPokelist] = useState([])
-
-  const fetchNewPokemon = async () => {
-    const data = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${
-        Math.floor(Math.random() * 932) + 1
-      }/`
-    )
-    const newPokemonData = await data.json()
-    console.log(newPokemonData)
-    setPokelist([newPokemonData, ...pokelist])
-  }
 
   return (
-    <HomeScreen onClick={fetchNewPokemon} pokelist={pokelist}/>
+    <HomeScreen/>
+    // <Router>
+    //   <Routes>
+    //   <Route path='/' component={HomeScreen} exact />
+    //   </Routes>
+    // </Router>
   )
 }
 
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Generate from './Generate'
 import MainList from './MainList'
+import { useState, useEffect } from 'react'
+
 
 const HomeScreen = (props) => {
+  useEffect(() => {}, [])
+  const [pokelist, setPokelist] = useState([])
+
+  const fetchNewPokemon = async () => {
+    const data = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${
+        Math.floor(Math.random() * 932) + 1
+      }/`
+    )
+    const newPokemonData = await data.json()
+    console.log(newPokemonData)
+    setPokelist([newPokemonData, ...pokelist])
+  }
+
+
     return (
         <div className='App' style={{ backgroundColor: '#004048' }}>
-        <Generate onClick={props.onClick} />
-        <MainList pokelist={props.pokelist} />
+        <Generate onClick={fetchNewPokemon} />
+        <MainList pokelist={pokelist} />
       </div>
     )
 }
