1609a7a18169b5c8340f6bef8eaf99e142692bf0
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2021-12-29 17:49:07 -0700

    labeled images

--- src/DetailScreen.js
+++ src/DetailScreen.js
@@ -10,7 +10,7 @@ const DetailScreen = () => {
   const [pokeData, setPokeData] = useState({})
   useEffect(() => {
     fetchPokeData()
-  }, [pokeData])
+  }, [])
 
   // console.log(pokeData)
 
@@ -30,24 +30,26 @@ const DetailScreen = () => {
         <Button>Return</Button>
       </Link>
       <Container>
-        <h3>{pokeData?.name?.charAt(0)?.toUpperCase() + pokeData?.name?.slice(1)}</h3>
+        <h3>
+          {pokeData?.name?.charAt(0)?.toUpperCase() + pokeData?.name?.slice(1)}
+        </h3>
 
         <Row>
-            <Col>
-              <Image src={pokeData?.sprites?.front_default} width='150' />
-            </Col>
-            <Col>
-              <Image src={pokeData?.sprites?.back_default} width='150' />
-            </Col>
-            <Col>
-              <Image src={pokeData?.sprites?.front_shiny} width='150' />
-            </Col>
-            <Col>
-              <Image src={pokeData?.sprites?.back_shiny} width='150' />
-            </Col>
+          <Col>
+            <h6>Normal</h6>
+            <Image src={pokeData?.sprites?.front_default} width='150' />
+          </Col>
+          <Col>
+            <Image src={pokeData?.sprites?.back_default} width='150' />
+          </Col>
+          <Col>
+            <h6>Shiny</h6>
+            <Image src={pokeData?.sprites?.front_shiny} width='150' />
+          </Col>
+          <Col>
+            <Image src={pokeData?.sprites?.back_shiny} width='150' />
+          </Col>
         </Row>
-
-
       </Container>
     </div>
   )
